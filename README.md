7a2585c6b7b99b15ae95cb347e42ce88415554fd
DarshanGaviyappa <gaviyappa.d@northeastern.edu>
2024-10-17 14:29:33 -0400

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# Web---Assignment-6
+# INFO6150-Assignment6
+### Simple Weather App with HTML, SCSS and Js
+
+<details>
+ <summary> CSS Grid layout - 1</summary> 
+   </br>
+   Grid 1 implemented in the index.html file and 
+</details>
+<details>
+ <summary> CSS Grid layout - 2</summary> 
+   </br>
+   Grid 2 implemented in the index.html file 
+</details>
+<details>
+ <summary> Flex Grid layout - 1</summary> 
+   </br>
+ Grid 1 implemented in the flex-layout.html file and 
+</details>
+<details>
+ <summary> Flex Grid layout - 2</summary> 
+   </br>
+   Grid 2 implemented in the flex-layout.html file  
+</details>
+
+<summary>
+
+- Variables: Used variables for ccomputing colors and sizes.
+- Custom Properties: 1. $margin for sizes in em.
+- Nesting : Example - .showcase { ... h1{ /*code*/} }
+- Interpolation: Implemented Interpolation for #{$image} & #{$margin}
+- Placeholder Selectors : Implmented for buttons in - $btn
+- Mixins : Implemented Mixins at _config.scss  to set background color for the page.
+- Functions : Implemented the @function to set text color of the page.
+- Extra features - 1.Calc : Implemented in the scss files to calcualte the margin based on the em values.
+                  2.@extend : Implemented in buttons for the flex layout page to mimic the type of buttons in the pages.
+                  3.Operators : Implmented the * (multiply) or / (divide) operators to calcualte the height or width of certain properties of cards and the body. 
---
+++ script.js
@@ -0,0 +1,123 @@
+const apiKey = "a40a5e83ca510b6857fa68548fb1236a";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const weatherIcon = document.querySelector(".weather-icon");
+let weatherConditionDisplay = document.querySelector(".weather-condition");
+function Dateandtime(){
+   var myDate = new Date();
+   var hrs = myDate.getHours();
+   var greet;
+   
+   if (hrs >= 5 && hrs < 11)
+       greet = 'Hello Good morning, Have a Great Day ';
+   else if (hrs >= 11 && hrs <= 17)
+       greet = 'Hello Good Afternoon';
+   else if (hrs >= 17 && hrs <= 23)
+       greet = 'Hello Good evening, How was your day? ';
+   else if (hrs == 24 || hrs < 5)
+       greet = 'Hello, night-owl.';
+       
+   document.getElementById('Greetings').innerHTML = greet;
+}
+
+function test(){
+
+var insidediv = "";
+var yo = " temoppp";
+var hum = "Humidity:"
+var win = "WindSpeed: "
+for (var i = 0; i < 9; i++) {
+   // insidediv += '<div class="city'+i+'">City:</div> <div class="temp'+i+'">temp</div> <div class="humidity'+i+'">Humidity</div> <div class="wind'+i+'">wind</div>';
+   insidediv += '<div class="insidecard"> <img src="" class="weather-icon'+i+'"/> <h1 class="temp'+i+'">test</h1><h2 class="city'+i+'">test</h2><p class="humidity'+i+'">test</p><p class="wind'+i+'">test</p>  <p class="weather-condition'+i+'"></p> </div>';
+}
+var container = document.getElementById("container");
+container.innerHTML = insidediv;
+// dont use checkWeather(cities);
+
+}
+
+
+
+
+async function checkWeather(){
+   const cities = ["Bengaluru", "Boston", "New York City", "London", "Paris", "Delhi", "Hyderabad", "Doha", "Dubai"];
+
+   if(document.getElementsByClassName("city8")){
+      // alert("created");
+      console.log(cities[0]);
+   }
+   let i = 0
+   for(i=0;i<9; i++){
+   const response = await fetch(apiUrl + cities[i] + `&appid=${apiKey}`);
+   var data = await response.json();
+   let weatherConditionData = data.weather[0].main;
+   // var weatherConditionData = 'Clouds'
+
+   console.log(data);
+
+   document.querySelector(".city"+i).innerHTML = data.name;
+   document.querySelector(".temp"+i).innerHTML = data.main.temp + "°C";
+   document.querySelector(".humidity"+i).innerHTML = data.main.humidity + "%";
+   document.querySelector(".wind"+i).innerHTML = data.wind.speed + "km/h";
+   document.querySelector(".weather-condition"+i).innerText = `${weatherConditionData}`;
+   var testing = document.querySelector(".weather-icon"+i);
+console.log("weater"+weatherConditionData);
+        // update weather icon
+
+        switch (weatherConditionData) {
+            case 'Clouds':
+               document.querySelector(".weather-icon"+i).src = "https://raw.githubusercontent.com/sreedhar-shreyas/INFO6150-Assignment5/main/images/Clouds.png";
+            case 'Tornado':
+               document.querySelector(".weather-icon"+i).src = "https://raw.githubusercontent.com/sreedhar-shreyas/INFO6150-Assignment5/main/images/Clouds.png";
+                break;
+            case 'Rain':
+               document.querySelector(".weather-icon"+i).src = "https://raw.githubusercontent.com/sreedhar-shreyas/INFO6150-Assignment5/main/images/Rain.png";
+                break;
+            case 'Drizzle':
+               document.querySelector(".weather-icon"+i).src = "https://raw.githubusercontent.com/sreedhar-shreyas/INFO6150-Assignment5/main/images/Rain.png";
+                break;
+            case 'Clear':
+               document.querySelector(".weather-icon"+i).src = "https://raw.githubusercontent.com/sreedhar-shreyas/INFO6150-Assignment5/main/images/Sun.png";
+                break;
+            case 'Thunderstorm':
+               document.querySelector(".weather-icon"+i).src = "https://raw.githubusercontent.com/sreedhar-shreyas/INFO6150-Assignment5/main/images/Thunderstorm.png";
+                break;
+            case 'Snow':
+               document.querySelector(".weather-icon"+i).src = "https://raw.githubusercontent.com/sreedhar-shreyas/INFO6150-Assignment5/main/images/Snow.png";
+                break;
+            case 'Mist':
+            case 'Fog':
+            case 'Smoke':
+            case 'haze':
+               document.querySelector(".weather-icon"+i).src = 'https://raw.githubusercontent.com/sreedhar-shreyas/INFO6150-Assignment5/main/images/Mist.png';
+                break;
+            default:
+               document.querySelector(".weather-icon"+i).src = "https://raw.githubusercontent.com/sreedhar-shreyas/INFO6150-Assignment5/main/images/Clouds.png";
+        }
+}
+}
+Dateandtime();
+test();
+checkWeather();
+
+// async function checkWeather(cities){
+//     let i = 1;
+//     alert("idk");
+//    //  for(i in cities){
+//     const response = await fetch(apiUrl + cities1 + `&appid=${apiKey}`);
+//     var data = await response.json();
+//  console.log(data);alert("idk");
+
+//  document.querySelector("city0").innerHTML = data.name;
+// //  document.querySelector(".temp"+i).innerHTML = Math.round(data.main.temp) + "°C";
+// //  document.querySelector(".humidity"+i).innerHTML = data.main.humidity + "%";
+// //  document.querySelector(".wind"+i).innerHTML = data.wind.speed + "km/h";
+
+
+
+
+// }
+
+
+
+
